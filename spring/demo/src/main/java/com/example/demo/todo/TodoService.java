734a942951266866748f3da2b1d6b980a1a25ec8
RuriDesk <fukukazu.dev@gmail.com>
2026-04-18 01:29:11 +0900

    TodoService err fix

--- spring/demo/src/main/java/com/example/demo/todo/TodoService.java
+++ spring/demo/src/main/java/com/example/demo/todo/TodoService.java
@@ -1,13 +1,17 @@
 package com.example.demo.todo;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.todo.dto.TodoDto;
 import com.example.demo.todo.entity.TodoEntity;
 import com.example.demo.todo.repository.TodoRepository;
 
 @Service
+@Transactional  //更新処理が途中で失敗した場合でもロールバック
 public class TodoService {
     
     private final TodoRepository repository;
@@ -20,23 +24,28 @@ public class TodoService {
     public TodoDto create(TodoDto dto){
         TodoEntity entity = new TodoEntity(dto.getTitle(), dto.isDone());
         TodoEntity saved = repository.save(entity);
-        return new TodoDto(saved.getId(), saved.getTitle(), saved.isDone());
+        return toDto(saved);
     }
 
     //全件取得
     public List<TodoDto> getAll(){
-        return repository.findAll().stream()
-                .map(e -> new TodoDto(e.getId(), e.getTitle(), e.isDone()))
-                .toList();
+        //repository.findAll()はList<TodoEntity>を返す。これをList<TodoDto>に変換するためにストリームAPIを使用している
+        return repository.findAll().stream() // findAllで全件取得、stream()で1件ずつ処理するモードへ
+                .map(this::toDto)   //map()でEntity→DTOに変換する処理を適用
+                .collect(Collectors.toList());  //ToList()でListに戻す
     }
 
     //更新処理
     public TodoDto update(Long id, TodoDto dto){
-        TodoEntity entity = repository.findById(id).orElseThrow();
+        //orElseThrowは例外の種類がNoSuchElementExceptionであるため、RuntimeExceptionに変更
+        TodoEntity entity = repository.findById(id)
+        //orElseThrow()は値が無い場合に例外を投げる
+               .orElseThrow(() -> new RuntimeException("Todo not found " + id));
         entity.setTitle(dto.getTitle());
         entity.setDone(dto.isDone());
+
         TodoEntity updated = repository.save(entity);
-        return new TodoDto(updated.getId(), updated.getTitle(), updated.isDone());
+        return toDto(updated);
     }
 
     //削除処理
@@ -44,4 +53,8 @@ public class TodoService {
         repository.deleteById(id);
     }
     
-}
+    //DTO変換をメソッド化、コードの重複を減らし、保守性も上げる
+    private TodoDto toDto(TodoEntity e ){
+        return new TodoDto(e.getId(), e.getTitle(), e.isDone());
+    }
+}   
